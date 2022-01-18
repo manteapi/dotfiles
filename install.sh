312c6752316d7696ec8fb92981734ad66653f2be
Pierre-Luc Manteaux <pierre-luc.manteaux@circle.dental>
2022-01-18 15:27:17 +0100

    Add dummy shell script to install desired dotfiles

---
+++ install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+cd ./i3
+sh install.sh
+cd ..
+
+cd ./git
+sh install.sh
+cd ..
+
+cd ./zsh
+sh install.sh
+cd ..
+
+cd ./nvim
+sh install.sh
+cd ..
+
+cd ./sddm
+sh install.sh
+cd ..
+
+cd ./tmux
+sh install.sh
+cd ..
+
+cd ./dunst
+sh install.sh
+cd ..
+
+cd ./fonts
+sh install.sh
+cd ..
+
+cd ./ranger
+sh install.sh
+cd ..
+
+cd ./sheldon
+sh install.sh
+cd ..
+
+cd ./neofetch
+sh install.sh
+cd ..
+
+cd ./alacritty
+sh install.sh
+cd ..
+
+cd ./justfiles
+sh install.sh
+cd ..
---
+++ uninstall.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+cd ./i3
+sh uninstall.sh
+cd ..
+
+cd ./git
+sh uninstall.sh
+cd ..
+
+cd ./zsh
+sh uninstall.sh
+cd ..
+
+cd ./nvim
+sh uninstall.sh
+cd ..
+
+cd ./sddm
+sh uninstall.sh
+cd ..
+
+cd ./tmux
+sh uninstall.sh
+cd ..
+
+cd ./dunst
+sh uninstall.sh
+cd ..
+
+cd ./fonts
+sh uninstall.sh
+cd ..
+
+cd ./ranger
+sh uninstall.sh
+cd ..
+
+cd ./sheldon
+sh uninstall.sh
+cd ..
+
+cd ./neofetch
+sh uninstall.sh
+cd ..
+
+cd ./alacritty
+sh uninstall.sh
+cd ..
+
+cd ./justfiles
+sh uninstall.sh
+cd ..
